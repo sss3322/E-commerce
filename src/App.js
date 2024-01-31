d06e4bb96aee5c052884f02ec5259f238f2cfcd3
sss3322 <suchithrashanmukhan@gmail.com>
2024-01-31 20:24:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
+
+import Cart from './Cart';
+import Home from './Home';
+import { CartProvider } from './CartContext';
+import ProductDetails from './ProductDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/product/:id" element={<ProductDetails/>} />
+        </Routes>
+      </Router>
+      </CartProvider>
+    
   );
 }
 
